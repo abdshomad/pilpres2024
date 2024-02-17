771b9b1d67169ca041ee2046177bfdb5696d05f9
Abd Shomad <abd.shomad@gmail.com>
2024-02-17 09:14:03 +0700

    running script 01-get-kabupaten-kota-json.py, ambil data kabupaten/kota

--- 01-get-kabupaten-kota-json.py
+++ 01-get-kabupaten-kota-json.py
@@ -12,22 +12,25 @@ with open(file_path, "r") as f:
 
 # Convert the JSON data into a Pandas DataFrame
 propinsi_df = pd.DataFrame(propinsi_json)
+propinsi_df = propinsi_df.sort_values(by='kode', ascending=True)
 
 # Display the DataFrame
 print(propinsi_df)
 
-directory_path = './data/'
-for kode in propinsi_df['kode']:
-    url = 'https://sirekap-obj-data.kpu.go.id/wilayah/pemilu/ppwp/'+kode+'.json'
-    file_path = os.path.join(directory_path, kode + '.json')
+data_directory_path = './data/'
+for kode_kabupaten_kota in propinsi_df['kode']:
+    url = 'https://sirekap-obj-data.kpu.go.id/wilayah/pemilu/ppwp/'+kode_kabupaten_kota+'.json'
+    print('url', url)
+    file_path = os.path.join(data_directory_path, kode_kabupaten_kota + '.json')
+    print(file_path, file_path)
     if os.path.exists(file_path):
         print('File: ', file_path, ' Exists')
     else:
-        filename = wget.download(url)
-        print(f"File '{filename}' downloaded.")
+        downloaded_filename = wget.download(url, out=file_path)
+        print(f"File '{downloaded_filename}' downloaded.")
 
 # Define the directory containing the JSON files
-directory_path = './data/'
+data_directory_path = './data/'
 
 # Initialize an empty list to store the combined JSON data
 combined_data = []
@@ -35,7 +38,7 @@ combined_data = []
 # Iterate over the range of file numbers from 11 to 55
 for i in range(11, 99):
     # Construct the file path for each file
-    file_path = os.path.join(directory_path, f"{i}.json")
+    file_path = os.path.join(data_directory_path, f"{i}.json")
 
     # Check if the file exists
     if os.path.exists(file_path):
@@ -47,7 +50,7 @@ for i in range(11, 99):
             combined_data.extend(json_data)
 
 # Write the combined data to merged.json
-with open(directory_path + 'kabupaten_kota.json', 'w') as merged_file:
+with open(data_directory_path + 'kabupaten_kota.json', 'w') as merged_file:
     json.dump(combined_data, merged_file)
 
 print("Merged JSON data has been written to kabupaten_kota.json.")
